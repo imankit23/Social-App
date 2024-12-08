818451aab34dc4747075b461aee6a359309b404b
imankit23 <153224154+imankit23@users.noreply.github.com>
2024-12-08 20:09:26 +0530

    Update spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "4337bc0b72b84c5c8ebf242f7118f138";
-const redirectUri = "http://localhost:3000";
+const redirectUri = "https://social-app-woad-chi.vercel.app/callback";
 const scopes = ["user-library-read", "playlist-read-private"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
